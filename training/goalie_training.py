e6bedfc62dea68ed84cb7049439482791e9bf817
Sebastian <SebastianPrior@outlook.com>
2021-05-31 15:53:26 +0200

    Add goal target, copy basic graders for future customizing, set spawnId

--- training/goalie_training.py
+++ training/goalie_training.py
@@ -1,19 +1,50 @@
-from math import pi
-from dataclasses import dataclass
+from math import pi, pow, sqrt
+from dataclasses import dataclass, field
 
-from rlbottraining.common_exercises.common_base_exercises import GoalieExercise
+from rlbottraining.training_exercise import TrainingExercise
+from rlbottraining.grading.grader import Grader
 from rlbottraining.rng import SeededRandomNumberGenerator
 from rlbottraining.training_exercise import Playlist
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator
+from rlbottraining.common_graders.compound_grader import CompoundGrader
+from base_graders import PassOnBallGoingAwayFromGoal, PassOnGoalForAllyTeam, PassOnTimeout
+
+
+class GoalieGrader(CompoundGrader):
+    def __init__(self, timeout_seconds=10.0, ally_team=0):
+        super().__init__([
+            PassOnBallGoingAwayFromGoal(ally_team),
+            PassOnGoalForAllyTeam(ally_team),
+            PassOnTimeout(timeout_seconds),
+        ])
+
+
+def norm(vec):
+    norm = sqrt(pow(vec.x, 2)+pow(vec.y, 2) + pow(vec.z, 2))
+    return Vector3(vec.x / norm, vec.y/norm, vec.z/norm)
+
+
+def minus(vec1, vec2):
+    return Vector3(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z)
+
+
+def times(vec, scalar):
+    return Vector3(vec.x * scalar, vec.y * scalar, vec.z * scalar)
 
 
 @dataclass
-class BallRollingToGoalie(GoalieExercise):
+class BallRollingToGoalie(TrainingExercise):
+    grader: Grader = field(default_factory=GoalieGrader)
+
     def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
+        target = Vector3(rng.uniform(-800, 800), -5800, 0)
+        ball_location = Vector3(rng.uniform(-800, 800), 0, 100)
+        ball_velocity = times(
+            norm(minus(target, ball_location)), rng.uniform(1500, 1800))
         return GameState(
             ball=BallState(physics=Physics(
-                location=Vector3(rng.uniform(-30, 30), -1500, 100),
-                velocity=Vector3(0, rng.uniform(-500, -1000), 0),
+                location=ball_location,
+                velocity=ball_velocity,
                 angular_velocity=Vector3(0, 0, 0)
             )),
             cars={
@@ -24,8 +55,6 @@ class BallRollingToGoalie(GoalieExercise):
                         velocity=Vector3(0, 0, 0),
                         angular_velocity=Vector3(0, 0, 0)
                     ),
-                    jumped=False,
-                    double_jumped=False,
                     boost_amount=0
                 )
             },
