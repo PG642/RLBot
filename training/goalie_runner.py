9a8f997f7cce73059903f48825f686aebb3e5a5d
Hendrik Meyer <hendrik2.meyer@tu-dortmund.de>
2021-06-01 17:07:41 +0200

    Fix reloading of MatchConfig after each exercise #1
    Each time a exercise is started RLBot checks if the MatchConfig has changed. By assigning the same MatchConfig to all exercises the Game and Bot are not reseted each time a exercise is finished. This way the model is only loaded once before the first exercise.

--- training/goalie_runner.py
+++ training/goalie_runner.py
@@ -2,26 +2,35 @@ from pathlib import Path
 
 import rlbottraining.exercise_runner as er
 
-from rlbot.matchconfig.match_config import Team, PlayerConfig, MatchConfig
+from rlbot.matchconfig.conversions import read_match_config_from_file
+from rlbot.matchconfig.match_config import Team, PlayerConfig
 from rlbottraining.training_exercise import Playlist
 import goalie_training as gt
 
+
+
+def make_match_config():
+    match_config = read_match_config_from_file(Path('./goalie.cfg'))
+    playerConfig = PlayerConfig.bot_config(
+        Path(__file__).absolute().parent.parent / 'src' / 'bot.cfg', Team.BLUE)
+    # This is needed to only reset the gamestate instead of resetting the game
+    # Else the spawn_id will be random and each new spawn ID provokes a new gamestate
+    # playerConfig.spawn_id = 1
+    match_config.player_configs = [
+        playerConfig
+    ]
+    return match_config
+
+match_config = make_match_config()
 
 def make_default_playlist() -> Playlist:
     exercises = [
         gt.BallRollingToGoalie('BallRollingToGoalie')
     ]
     for exercise in exercises:
-        playerConfig = PlayerConfig.bot_config(
-            Path(__file__).absolute().parent.parent / 'src' / 'bot.cfg', Team.BLUE)
-        # This is needed to only reset the gamestate instead of resetting the game
-        # Else the spawn_id will be random and each new spawn ID provokes a new gamestate
-        playerConfig.spawn_id = 1
-        exercise.match_config.player_configs = [
-            playerConfig
-        ]
+        exercise.match_config = match_config
     return exercises
 
 
 if __name__ == "__main__":
-    er.run_module(Path(__file__).absolute())
+    er.run_module(Path(__file__).absolute(), reload_policy=er.ReloadPolicy.EACH_EXERCISE)
