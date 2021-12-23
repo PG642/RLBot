fd6a8b7d978f1c74e12a0433715deaaa19965326
Hendrik Meyer <hendrik2.meyer@tu-dortmund.de>
2021-12-23 14:45:12 +0100

    Add support for multi discrete actions spaces in Onnx model.

--- src/models/onnx_model.py
+++ src/models/onnx_model.py
@@ -1,17 +1,26 @@
 import onnx
 import onnxruntime
-
+import numpy as np
 
 class ONNXModel():
     def __init__(self, model_path):
         self.model_path = model_path
         self.model = onnx.load(model_path)
         self.session = onnxruntime.InferenceSession(model_path, None)
+
         self.input_name = self.session.get_inputs()[0].name
         self.output_name = self.session.get_outputs()[2].name
 
+        self.is_continuous = self.output_name == 'continuous_actions'
+        self.is_multi_discrete = self.output_name == 'discrete_actions'
+
     def run(self, data):
-        return self.session.run([self.output_name], {self.input_name: data})
+        if self.is_continuous:
+            return self.session.run([self.output_name], {self.input_name: data})
+        elif self.is_multi_discrete:
+            return self.session.run([self.output_name], {self.input_name: data, 'action_masks': np.array([[1] * 26], dtype=np.float32)})
+        else:
+            raise NotImplementedError
 
     def check(self):
         try:
